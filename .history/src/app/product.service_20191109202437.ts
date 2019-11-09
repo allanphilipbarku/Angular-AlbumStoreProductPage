dd404a18f345d6e6072522dab61520e242f66201
Allan Philip Barku <allanbarku@gmail.com>
2019-11-09 20:32:13 +0000

    Using Interfaces to Describe Data

--- .history/src/app/product.service_20191109202437.ts
+++ .history/src/app/product.service_20191109202437.ts
@@ -3,15 +3,13 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
 import 'rxjs/add/operator/map';
-import { Observable } from 'rxjs/Observable';
-import { Album } from './album';
 
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
   constructor(private _http: Http) {}
 
-  getAlbum(id: number): Observable<Album> {
-    return this._http.get(this._albumUrl).map(response => <Album>response.json());
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl: Observable<Album>).map(response => response.json());
   }
 }
