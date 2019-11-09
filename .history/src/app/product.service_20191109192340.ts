e04e0e56d7ca8c5fa63e1003bd0b00c329531b46
Allan Philip Barku <allanbarku@gmail.com>
2019-11-09 20:09:30 +0000

    Creating a Service to Get Album Data

---
+++ .history/src/app/product.service_20191109192340.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@angular/core";
+
+import { Http, Response } from "@angular/http";
+
+@Injectable()
+export class ProductService {
+  _albumUrl = "../assets/album.json";
+  constructor(private _http: Http) {}
+
+  getAlbum(id: number) {}
+}
